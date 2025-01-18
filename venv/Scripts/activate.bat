aa11ed41be54d57dcd65f79a1a94debab391105f
Archit-29 <archetsrivastav056@gmail.com>
2025-01-18 22:37:58 +0530

    final  commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\webdev\topsis\venv
+set VIRTUAL_ENV=C:\webdev\topsis_102203821\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
